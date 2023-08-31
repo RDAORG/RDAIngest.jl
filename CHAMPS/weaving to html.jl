a95c039d5b39bc5ddaf2d9b6f9b33e1e0fda5589
Yoonyoung Choi <56310837+young-wild-free@users.noreply.github.com>
2023-08-31 16:26:45 +0200

    d

--- CHAMPS/weaving to html.jl
+++ CHAMPS/weaving to html.jl
@@ -3,7 +3,7 @@ Pkg.add("Weave")
 using Weave
 
 
-filename = normpath("/Users/young/Documents/GitHub/RDAIngest.jl/documentations", "CHAMPS_VA_Quality_Report.jmd")
+filename = normpath("/Users/young/Documents/GitHub/RDAIngest.jl/CHAMPS", "CHAMPS_VA_Quality_Report.jmd")
 
 # Julia markdown to HTML
 weave(filename; doctype = "md2html", out_path = :pwd)
